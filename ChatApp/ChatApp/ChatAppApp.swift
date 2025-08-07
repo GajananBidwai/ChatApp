3a39fe4d7108f4cbb0c1728353c13efe653e4321
Gajanan Bidwai <gajanan@vyom.ai>
2025-08-07 13:17:09 +0530

    Initial Commit
    First commit with empty code

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Neosoft on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
